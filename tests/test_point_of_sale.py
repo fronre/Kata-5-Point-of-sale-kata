85d6ea6141e55d4f00ddae51a30a42ae99148f93
fronre <islamhala35@gmail.com>
2025-11-04 17:23:14 +0100

    testing

--- tests/test_point_of_sale.py
+++ tests/test_point_of_sale.py
@@ -6,6 +6,11 @@ from src.point_of_sale import point_of_sale
     ("23456", "$12.50"),
     ("99999", "Error: barcode not found"),
     ("", "Error: empty barcode"),
+    ("888888", "Error: barcode not found"),
+    ("00000", "Error: barcode not found"),
+    ("1234a", "Error: barcode not found"),
+    ("TOTAL", "Error: barcode not found"),
+
 ])
 def test_point_of_sale_various_cases(barcode, expected):
     assert point_of_sale(barcode) == expected
