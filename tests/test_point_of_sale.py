fc985bf17c1effc493bddf9c9adc2fd275ecc90c
fronre <islamhala35@gmail.com>
2025-11-04 17:07:05 +0100

    failed test

--- tests/test_point_of_sale.py
+++ tests/test_point_of_sale.py
@@ -2,3 +2,6 @@ from src.point_of_sale import point_of_sale
 
 def test_point_of_sale_input_empty_return_error():
     assert point_of_sale("") == "Error: barcode is empty"
+
+def test_point_of_sale_barcode_12345_should_return_price():
+    assert point_of_sale("12345") == "$7.25"
